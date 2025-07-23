4ba24b0859bf738c9265e75eb37a7fabe7e29083
Maxim Samsonov <m.samsonov@computer.org>
2025-07-23 23:48:45 +0300

    wip

--- Fuelflux.Core/Authorization/JwtUtils.cs
+++ Fuelflux.Core/Authorization/JwtUtils.cs
@@ -23,7 +23,6 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
