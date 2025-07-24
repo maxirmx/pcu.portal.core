61acd8c7ef06987c66bc0ebefb6d3db5a2f1fdbd
Maxim Samsonov <m.samsonov@computer.org>
2025-07-24 13:15:01 +0300

    fix review notes

--- Fuelflux.Core/Services/DeviceAuthService.cs
+++ Fuelflux.Core/Services/DeviceAuthService.cs
@@ -1,3 +1,28 @@
+// Copyright (C) 2025 Maxim [maxirmx] Samsonov (www.sw.consulting)
+// All rights reserved.
+// This file is a part of Fuelflux Core application
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 using System.Collections.Concurrent;
 using System.Security.Cryptography;
 using Microsoft.Extensions.Options;
@@ -22,7 +47,9 @@ public class DeviceAuthService : IDeviceAuthService
         var token = Convert.ToHexString(RandomNumberGenerator.GetBytes(16));
         var expires = DateTime.UtcNow.AddMinutes(_settings.SessionMinutes);
         _sessions[token] = expires;
-        _logger.LogDebug("Token {token} authorized until {expires}", token, expires);
+        
+        var tokenPrefix = GetSafeTokenIdentifier(token);
+        _logger.LogDebug("Token {tokenPrefix} authorized until {expires}", tokenPrefix, expires);
         return token;
     }
 
@@ -35,7 +62,9 @@ public class DeviceAuthService : IDeviceAuthService
                 return true;
             }
             _sessions.TryRemove(token, out _);
-            _logger.LogDebug("Token {token} expired", token);
+            
+            var tokenPrefix = GetSafeTokenIdentifier(token);
+            _logger.LogDebug("Token {tokenPrefix} expired", tokenPrefix);
         }
         return false;
     }
@@ -43,6 +72,13 @@ public class DeviceAuthService : IDeviceAuthService
     public void Deauthorize(string token)
     {
         _sessions.TryRemove(token, out _);
-        _logger.LogDebug("Token {token} deauthorized", token);
+        
+        var tokenPrefix = GetSafeTokenIdentifier(token);
+        _logger.LogDebug("Token {tokenPrefix} deauthorized", tokenPrefix);
+    }
+
+    private static string GetSafeTokenIdentifier(string token)
+    {
+        return token.Length > 8 ? $"{token[..8]}..." : "short_token";
     }
 }
