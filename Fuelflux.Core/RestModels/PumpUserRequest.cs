ecd0a68eff135d56fbcfc7f43657672d43b6d99e
Maxim Samsonov <m.samsonov@computer.org>
2025-07-25 12:31:45 +0300

    clean

--- Fuelflux.Core/RestModels/PumpUserRequest.cs
+++ Fuelflux.Core/RestModels/PumpUserRequest.cs
@@ -4,9 +4,9 @@ namespace Fuelflux.Core.RestModels;
 
 public class PumpUserRequest
 {
-    [Range(0, int.MaxValue, ErrorMessage = "first must be non-negative.")]
+    [Range(0, int.MaxValue, ErrorMessage = "начальное значение не может быть отрицательным.")]
     public int First { get; set; }
 
-    [Range(1, int.MaxValue, ErrorMessage = "number must be positive.")]
+    [Range(1, int.MaxValue, ErrorMessage = "количество должно быть положительным.")]
     public int Number { get; set; }
 }
